31148557bf4da47cb57d4c71a8d95ca7d65ef65d
Juliane T. Cobiank <72655764+kagumiih@users.noreply.github.com>
2023-03-07 19:42:43 -0300

    Add files via upload

---
+++ Exercicio3/Dados.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public class Dados
+    {
+        public int Dia { get; set; }
+        public float Valor { get; set; }
+
+        public float MenorValor(List<Dados> listadados)
+        {
+            var listafiltrada = Filtragem(listadados);
+           return listafiltrada.Min(x => x.Valor);
+        }
+        public float MaiorValor(List<Dados> listadados)
+        {
+            var listafiltrada = Filtragem(listadados);
+            return listafiltrada.Max(x => x.Valor);
+        }
+        public int ValorSuperior(List<Dados> listadados)
+        {
+            var listafiltrada = Filtragem(listadados);
+            var media = listafiltrada.Sum(x=> x.Valor) / listafiltrada.Count();
+            return listafiltrada.Where(x => x.Valor >= media).ToList().Count();
+        }
+
+        private List<Dados> Filtragem(List<Dados> listadados)
+        {
+           return listadados.Where(x => x.Valor != 0).ToList();
+        }
+    }
+}
---
+++ Exercicio3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Exercicio3;
+using Newtonsoft.Json;
+class Program{
+static void Main(){
+
+        string jsonString = File.ReadAllText($@"{Environment.CurrentDirectory}\dados.json");
+
+        // Desserializar o JSON em um objeto C#
+        var listadados = JsonConvert.DeserializeObject<List<Dados>>(jsonString);
+        var dados = new Dados();
+        
+        Console.WriteLine("Maior valor: "+dados.MaiorValor(listadados));
+        Console.WriteLine("Menor valor: "+dados.MenorValor(listadados));
+        Console.WriteLine("Numero de dias: "+dados.ValorSuperior(listadados));
+    }
+}
