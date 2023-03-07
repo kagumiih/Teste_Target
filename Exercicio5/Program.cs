78136cbf0c845f1c3c2f70ab9772d36c22bb7931
Juliane T. Cobiank <72655764+kagumiih@users.noreply.github.com>
2023-03-07 19:43:57 -0300

    Add files via upload

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program{
+    static void Main(){
+        Console.WriteLine("Digite a string: ");
+        var stringNormal = Console.ReadLine();
+        Console.WriteLine("Sua String invertida: "+InverteString(stringNormal));
+    }
+    public static string InverteString(string str){
+        char[] charArray = str.ToCharArray();
+        int i = 0, j = charArray.Length - 1;
+
+        while (i < j)
+        {
+            char temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+
+            i++;
+            j--;
+        }
+        return new string(charArray);
+    }
+
+}
