98dfc69bbc66e6ac59e8fb3adb6b2778ce66590b
Juliane T. Cobiank <72655764+kagumiih@users.noreply.github.com>
2023-03-07 19:43:24 -0300

    Add files via upload

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program{
+    static void Main(){
+        const double SP = 67836.43;
+        const double RJ = 36678.66;
+        const double MG = 29229.88;
+        const double ES = 27165.48;
+        const double outros = 19849.53;
+
+        var total = SP + RJ + MG + ES + outros;
+        Console.WriteLine("Percentual SP: "+Percentual(SP,total)+"%");
+        Console.WriteLine("Percentual RJ: "+Percentual(RJ,total)+"%");
+        Console.WriteLine("Percentual MG: "+Percentual(MG,total)+"%");
+        Console.WriteLine("Percentual ES: "+Percentual(ES,total)+"%");
+        Console.WriteLine("Percentual Outros: "+Percentual(outros,total)+"%");
+
+    }
+    private static double Percentual(double valor, double total){
+        var porcento = (100 * valor) / total;
+        return Math.Round(porcento,2);
+    }
+}
